8d0c20a5eeae0d97ec4c8dee443c6dc8579f5041
Paymon <paymon@geeks.ltd.uk>
2017-11-26 15:48:36 +0000

    Initialization fix

--- Template/Initialize.bat
+++ Template/Initialize.bat
@@ -1,16 +1,35 @@
 
+@echo off
+
 cd @M#\@Model
-dotnet build
+call dotnet build
 
 cd ..\..\Domain
-dotnet build
+call dotnet build
 
 cd ..\Website
-wwwroot\Styles\build\Sass-to-css.bat
+
+ECHO Checking bower is installed globally..............
+where bower > nul
+if ERRORLEVEL 1 (
+	echo Installing bower globally...
+	npm install bower -g	
+)
+
+ECHO Running NPM ..............
+call npm install
+
+ECHO Running BOWER ..............
+call bower install
+
+ECHO Rebuilding node-sass module ..............
+call npm rebuild node-sass
+
+ECHO Running node-sass ..............
+call node_modules\.bin\gulp build-sass
 
 cd Website
-dotnet build
+call dotnet build
 
 cd ..\@M#\@UI
-dotnet build
-
+call dotnet build
