f3201f0eeefe5c5d25f1c57625e8a2c7b382be87
Mohsen Seifi <mohsens2@outlook.com>
2018-01-29 16:29:42 +0330

    Added swagger support Changes docker

---
+++ Template/Website/Controllers/Api/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public Tst Get()
+        {
+            return new Tst();
+        }
+    }
+    public class Tst
+    {
+        public Tst()
+        {
+            this.Message = "Hello world";
+        }
+        public string Message { get; set; }
+    }
+}
--- Template/Website/app_Start/Startup.cs
+++ Template/Website/app_Start/Startup.cs
@@ -1,6 +1,7 @@
 ﻿namespace Website
 {
     using System.Globalization;
+    using System.Linq;
     using System.Threading.Tasks;
     using Domain;
     using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@
     using Olive;
     using Olive.Services.Hangfire;
     using Olive.Services.Testing;
+    using Swashbuckle.AspNetCore.Swagger;
 
     public class Startup : Olive.Mvc.Startup
     {
@@ -20,12 +22,38 @@
 
             AuthenticationBuilder.AddSocialAuth();
             services.AddScheduledTasks();
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Olive API", Version = "v1" });
+                c.DocInclusionPredicate((docName, apiDesc) =>
+                {
+                    if (apiDesc.HttpMethod == null) return false;
+                    var a = apiDesc.SupportedResponseTypes;
+                    if (a.Any())
+                    {
+                        return true;
+                    }
+                    return false;
+                });
+                
+            });
         }
 
         public override void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseWebTest(ReferenceData.Create, config => config.AddTasks());
             base.Configure(app, env);
+
+            //app.UseStaticFiles();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Olive API V1");
+            });
 
             if (Config.Get<bool>("Automated.Tasks:Enabled"))
                 app.UseScheduledTasks(TaskManager.Run);
