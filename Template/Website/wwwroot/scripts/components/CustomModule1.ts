8d3fc1e41273f52ab50e616e1acdbf26d86032f5
Paymon <paymon@geeks.ltd.uk>
2018-06-04 16:51:44 +0100

    no message

--- Template/Website/wwwroot/scripts/components/CustomModule1.ts
+++ Template/Website/wwwroot/scripts/components/CustomModule1.ts
@@ -1,6 +1,6 @@
 ﻿
-// JS TO LOAD THIS ----> loadModule('/scripts/components/CustomModule1', m => m.default.Run());
-// OR, IN AN M# PAGE ----> LoadJavascriptModule("/scripts/components/CustomModule1", "Run()");
+// JS TO LOAD THIS ----> loadModule('/scripts/components/CustomModule1', m => m.default.run());
+// OR, IN AN M# PAGE ----> LoadJavascriptModule("/scripts/components/CustomModule1", "run()");
 
 // Dependencies:
 import AppPage from "../AppPage"
@@ -9,7 +9,7 @@ export default class CustomModule1 {
 
     static get page(): AppPage { return window["page"]; }
 
-    public static Run(): void {
+    public static run(): void {
         console.log("Hello world! I am Custom-Module-1.");
 
         // Note: You can use << this.page >> to hook into the page lifecycle events,
