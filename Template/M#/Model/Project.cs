3e9ce24deb7e414bb1072cc3f113a25daf5a28e1
PaymonK <35069374+PaymonK@users.noreply.github.com>
2021-07-01 22:07:44 +0100

    Update Project.cs

--- Template/M#/Model/Project.cs
+++ Template/M#/Model/Project.cs
@@ -13,7 +13,7 @@ namespace App
             Role("Admin").SkipQueryStringSecurity();
 
             Layout("Admin default").AjaxRedirect().Default().VirtualPath("~/Views/Layouts/AdminDefault.cshtml");
-            Layout("Admin default Modal").Modal().VirtualPath("~/Views/Layouts/AdminDefault.Modal.cshtml");
+            Layout("Admin default Modal").AjaxRedirect().Modal().VirtualPath("~/Views/Layouts/AdminDefault.Modal.cshtml");
             Layout("Login").AjaxRedirect().VirtualPath("~/Views/Layouts/Login.cshtml");
             Layout("Blank").AjaxRedirect().VirtualPath("~/Views/Layouts/Blank.cshtml");
 
