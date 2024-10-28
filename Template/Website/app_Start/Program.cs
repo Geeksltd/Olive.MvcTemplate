26ac21c3c16998ade32ba03b6a8f1b9b4fb6a010
matt_geeksltd <matt@geeks.ltd.uk>
2024-10-28 13:41:59 +0000

    no message

--- Template/Website/app_Start/Program.cs
+++ Template/Website/app_Start/Program.cs
@@ -2,9 +2,11 @@
 {
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
     using Olive;
+    using System;
 
     public class Program
     {
@@ -13,9 +15,21 @@
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
-                .ConfigureLogging(ConfigureLogging)
-                .UseSetting("detailedErrors", "true").CaptureStartupErrors(true)
-                .UseStartup<Startup>();
+              .ConfigureLogging(ConfigureLogging)
+              .ConfigureAppConfiguration((context, config) =>
+              {
+                  var machineName = Environment.MachineName;
+                  var env = context.HostingEnvironment;
+                  config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                       // Load environment-specific appsettings.{Environment}.json (e.g., Development, Production)
+                       .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                       // Load machine-specific appsettings.{machineName}.json
+                       .AddJsonFile($"appsettings.{machineName}.json", optional: true, reloadOnChange: true);
+
+              })
+              .UseSetting("detailedErrors", "true")
+              .CaptureStartupErrors(true)
+              .UseStartup<Startup>();
         }
 
         static void ConfigureLogging(WebHostBuilderContext context, ILoggingBuilder logging)
